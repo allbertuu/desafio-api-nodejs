781301e410eb255fe666a5f5d9f1e56f544cf6b9
Alberto <albertovinicius3@gmail.com>
2025-10-26 11:47:10 -0300

    Adiciona comentários

--- src/database/client.ts
+++ src/database/client.ts
@@ -1,5 +1,5 @@
-import { drizzle } from 'drizzle-orm/node-postgres'
+import { drizzle } from "drizzle-orm/node-postgres";
 
 export const db = drizzle(process.env.DATABASE_URL, {
-  logger: process.env.NODE_ENV === 'development',
-})
+  logger: process.env.NODE_ENV === "development", // exibe no console as queries em ambiente de desenvolvimento
+});
