5e07dbd875f12c4b54efb4200d7b9a9f872bea16
Alberto <albertovinicius3@gmail.com>
2025-10-26 11:47:42 -0300

    Formata o código em app.ts para melhorar a legibilidade

--- src/app.ts
+++ src/app.ts
@@ -1,46 +1,52 @@
-import fastify from 'fastify'
-import { fastifySwagger } from '@fastify/swagger'
-import { validatorCompiler, serializerCompiler, type ZodTypeProvider, jsonSchemaTransform } from 'fastify-type-provider-zod'
-import { createCourseRoute } from './routes/create-course.ts'
-import { getCourseByIdRoute } from './routes/get-course-by-id.ts'
-import { getCoursesRoute } from './routes/get-courses.ts'
-import scalarAPIReference from '@scalar/fastify-api-reference'
-import { loginRoute } from './routes/login.ts'
+import fastify from "fastify";
+import { fastifySwagger } from "@fastify/swagger";
+import {
+  validatorCompiler,
+  serializerCompiler,
+  type ZodTypeProvider,
+  jsonSchemaTransform,
+} from "fastify-type-provider-zod";
+import { createCourseRoute } from "./routes/create-course.ts";
+import { getCourseByIdRoute } from "./routes/get-course-by-id.ts";
+import { getCoursesRoute } from "./routes/get-courses.ts";
+import scalarAPIReference from "@scalar/fastify-api-reference";
+import { loginRoute } from "./routes/login.ts";
 
 const server = fastify({
   logger: {
     transport: {
-      target: 'pino-pretty',
+      target: "pino-pretty",
       options: {
-        translateTime: 'HH:MM:ss Z',
-        ignore: 'pid,hostname',
+        translateTime: "HH:MM:ss Z",
+        ignore: "pid,hostname",
       },
     },
   },
-}).withTypeProvider<ZodTypeProvider>()
+}).withTypeProvider<ZodTypeProvider>();
 
-if (process.env.NODE_ENV === 'development') {
+if (process.env.NODE_ENV === "development") {
   server.register(fastifySwagger, {
     openapi: {
       info: {
-        title: 'Desafio Node.js',
-        version: '1.0.0',
-      }
+        title: "Desafio Node.js",
+        version: "1.0.0",
+      },
     },
     transform: jsonSchemaTransform,
-  })
-  
+  });
+
   server.register(scalarAPIReference, {
-    routePrefix: '/docs',
-  })
+    routePrefix: "/docs",
+  });
 }
 
-server.setValidatorCompiler(validatorCompiler)
-server.setSerializerCompiler(serializerCompiler)
+server.setValidatorCompiler(validatorCompiler);
+server.setSerializerCompiler(serializerCompiler);
 
-server.register(createCourseRoute)
-server.register(getCourseByIdRoute)
-server.register(getCoursesRoute)
-server.register(loginRoute)
+// Registro das rotas
+server.register(createCourseRoute);
+server.register(getCourseByIdRoute);
+server.register(getCoursesRoute);
+server.register(loginRoute);
 
-export { server }
+export { server };
